33b593874644d7fd9f7e56098db6d18367582cc7
Dev <thomas.j.robson@hotmail.com>
2016-08-13 16:21:41 +1200

    completed ajax api JS doc, tweeked CSS and HTML, all working

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+document.addEventListener('DOMContentLoaded', init);
+
+function init () {
+  doIt();
+
+}
+
 function doIt() {
   var output = $.ajax({
       url: 'https://imtoobose-really-awful-poems-v1.p.mashape.com/poem', // The URL to the API. You can get this in the API page of the API you intend to consume
@@ -5,19 +12,13 @@ function doIt() {
       data: {}, // Additional parameters here
       dataType: 'json',
       success: function(data) {
-      	document.getElementById("output").innerHTML = data.source;
-        console.log(data)
         var poem = data.poem;
-        var author = data.author;
+        var author = data.author
+      	document.getElementById("thePoem").innerHTML = '" '+ author + ' "' +"<br>" + "<br>" + '" ' + poem + '. "';
+        console.log(data)
       },
     error: function(err) { alert(err); },
       beforeSend: function(xhr) {   xhr.setRequestHeader("X-Mashape-Authorization","4X7dQjipLbmsh0NhnAussclyCq53p1lJEWCjsnAtOsLW035rJ7"); // Enter here your Mashape key
       }
   });
   }
-
-
-
-function testButton() {
-    document.getElementById("button").innerHTML = "This is THE qoute";
-}
